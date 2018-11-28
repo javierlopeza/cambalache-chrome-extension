cba743f6a4c04d026ec34bcb74ef7dfd4a537747
Javier López <javierlopez@uc.cl>
2018-11-28 01:13:37 -0300

    Fix clock zero padding

--- js/newtab.js
+++ js/newtab.js
@@ -17,9 +17,11 @@ $(document).ready(async () => {
 	const now = new Date();
 	const hours = now.getHours();
 	const minutes = now.getMinutes();
-	$("#clock").text(`${hours}:${minutes}`);
+	$("#clock").text(`${zeroPad(hours, 2)}:${zeroPad(minutes, 2)}`);
 });
 
 const buildAuthorPath = author => `${BASE_URL}/authors/${author.kebabName}`;
 
 const buildBookPath = book => `${BASE_URL}/books/${book.isbn}`;
+
+const zeroPad = (n, z) => ("0".repeat(z) + n).slice(-z);
