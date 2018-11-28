2c25dd70ea4f17a954a3215ad58c6d84093722f6
Javier López <javierlopez@uc.cl>
2018-11-27 23:45:18 -0300

    Initial commit

---
+++ js/newtab.js
@@ -0,0 +1,25 @@
+// const BASE_URL = "http://localhost:3000"
+const BASE_URL = "http://cambalache.herokuapp.com"
+
+$(document).ready(async () => {
+	// Show random book
+	const response = await fetch(`${BASE_URL}/api/books/random`);
+	const body = await response.json();
+	const { book } = body;
+	const description = book.description.split(" ").slice(0, 40).join(" ") + " ...";
+	$("#book-description").text(description);
+	$("#book-author").text(book.author.name);
+	$("#book-author").attr("href", buildAuthorPath(book.author));
+	$("#image-link").prepend($("<img>",{ id: "book-image", src: book.imageUrl }));
+	$("#image-link").attr("href", buildBookPath(book));
+
+	// Show current time
+	const now = new Date();
+	const hours = now.getHours();
+	const minutes = now.getMinutes();
+	$("#clock").text(`${hours}:${minutes}`);
+});
+
+const buildAuthorPath = author => `${BASE_URL}/authors/${author.kebabName}`;
+
+const buildBookPath = book => `${BASE_URL}/books/${book.isbn}`;
